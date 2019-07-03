97070cc04b29bb183f8c215d2224b7f45138aa61
Sujitkumar Singh <2017.sujitkumar.singh@ves.ac.in>
2019-07-03 14:51:24 +0530

    all

--- index.js
+++ index.js
@@ -11,8 +11,19 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const server = http.createServer((req, res) => {
-	if (req.url == '/') {
-		res.end('<h1>Hello Home</h1>');
+	if (req.url === '/') {
+		fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+			if (err) throw err;
+			res.writeHead(200, { 'content-Type': 'text/html' });
+			res.end(content);
+		});
+	}
+	if (req.url === '/about') {
+		fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+			if (err) throw err;
+			res.writeHead(200, { 'content-Type': 'text/html' });
+			res.end(content);
+		});
 	}
 });
 const PORT = process.env.PORT || 5000;
