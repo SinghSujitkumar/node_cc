f5335e684c87541b28064cde8692da4b259011b3
Sujitkumar Singh <2017.sujitkumar.singh@ves.ac.in>
2019-07-03 13:56:33 +0530

    updated index

--- index.js
+++ index.js
@@ -7,3 +7,13 @@
 // const logger = new Logger();
 // logger.on('message', (data) => console.log('called Listener:', data));
 // logger.log('Hello Woled');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+	if (req.url == '/') {
+		res.end('<h1>Hello Home</h1>');
+	}
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port:${PORT}`));
